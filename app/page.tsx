f88bf9debf330f9ce796b44269fd98fb550aee9f
enzo <Eynzof@gmail.com>
2022-11-21 22:18:53 +0800

    switch to app dir

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
+import './globals.css'
 import Link from "next/link";
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from './Home.module.css'
 
 export default function Home() {
     return (
