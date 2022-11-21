8872431e2858229bc0b9219b77c36cad5c30fc93
enzo <Eynzof@gmail.com>
2022-11-21 18:24:06 +0800

    basic layout

---
+++ pages/flex.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/Flex.module.css';
+
+export default function flex() {
+    return(<div className={styles.main}>
+        <header className={styles.header}>Flex</header>
+        <div className={styles.sections}>
+            <section className={styles.section}>1</section>
+            <section className={styles.section}>2</section>
+            <section className={styles.section}>3</section>
+            <section className={styles.section}>4</section>
+            <section className={styles.section}>5</section>
+            <section className={styles.section}>6</section>
+        </div>
+    </div>)
+}
