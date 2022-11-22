a8530071345e56987c72792a06aeffdcb5cfd724
enzo <Eynzof@gmail.com>
2022-11-22 11:21:36 +0800

    separate grid template

--- app/grid/page.tsx
+++ app/grid/page.tsx
@@ -1,71 +1,59 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styles from "./grid.module.css";
-import Cube from "../../stories/Cube/Cube";
+import GridTemplate from "../../components/Grid/GridTemplate/GridTemplate";
 
 // @ts-ignore
 function Page() {
     return (
         <div className={styles.main}>
             <div className={styles.sets}>
+                <GridTemplate/>
                 <div className={styles.set}>
                     <div className={styles.header}>grid-template</div>
                     <div className={styles.lore}>
                         <p>Defines the rows and columns of the grid.</p>
                     </div>
                     <div className={styles.sections}>
-                        <Cube/>
-                        <Cube/>
-                        <Cube/>
-                        <Cube/>
-                    </div>
-                </div>
-                <div className={styles.set}>
-                    <div className={styles.header}>grid-template</div>
-                    <div className={styles.lore}>
-                        <p>Defines the rows and columns of the grid.</p>
-                    </div>
-                    <div className={styles.sections}>
-                        <div className="atom-blocks">
-                            <div className="atoms">
-                                {[...Array(9)].map((e, i) => i).map((i) => <div className="atom" key={i} style={{backgroundColor: `rgba(255, 241, 108, ${1-i*0.07})`}}></div>)}
+                        <div className={styles.blocks}>
+                            <div className={styles.grid}
+                                 style={{gridTemplateColumns: '12px 12px 12px', gridTemplateRows: '12px 12px 12px'}}>
+                                {[...Array(9)].map((e, i) => i).map((i) => <div className={styles.atom} key={i}
+                                                                                style={{backgroundColor: `rgba(255, 241, 108, ${1 - i * 0.07})`}}></div>)}
                             </div>
-                            <p className="atoms-heading">grid-template-columns: 12px 12px 12px;<br/>grid-template-rows: 12px 12px 12px;
+                            <p className={styles.header}>grid-template-columns: 12px 12px 12px;<br/>grid-template-rows:
+                                12px 12px 12px;
                             </p>
                         </div>
-                        <div className="atom-blocks">
-                            <div className="atoms">
-                                {[...Array(9)].map((e, i) => i).map((i) => <div className="atom" key={i} style={{backgroundColor: `rgba(255, 241, 108, ${1-i*0.07})`}}></div>)}
+                        <div className={styles.blocks}>
+                            <div className={styles.grid}
+                                 style={{gridTemplateColumns: 'repeat(3, 1fr)', gridTemplateRows: 'repeat(3, 1fr)'}}>
+                                {[...Array(9)].map((e, i) => i).map((i) => <div className={styles.atom} key={i}
+                                                                                style={{backgroundColor: `rgba(255, 241, 108, ${1 - i * 0.07})`}}></div>)}
                             </div>
-                            <p className="atoms-heading">grid-template-columns: repeat(3, 1fr);<br/>grid-template-rows: repeat(3, 1fr);
+                            <p className={styles.header}>grid-template-columns: repeat(3, 1fr);<br/>grid-template-columns:
+                                repeat(3, 1fr)
                             </p>
                         </div>
-                        <Cube/>
-                        <Cube/>
-                    </div>
-                </div>
-                <div className={styles.set}>
-                    <div className={styles.header}>grid-template</div>
-                    <div className={styles.lore}>
-                        <p>Defines the rows and columns of the grid.</p>
-                    </div>
-                    <div className={styles.sections}>
-                        <Cube/>
-                        <Cube/>
-                        <Cube/>
-                        <Cube/>
-                    </div>
-                </div>
-                <div className={styles.set}>
-                    <div className={styles.header}>grid-template</div>
-                    <div className={styles.lore}>
-                        <p>Defines the rows and columns of the grid.</p>
-                    </div>
-                    <div className={styles.sections}>
-                        <Cube/>
-                        <Cube/>
-                        <Cube/>
-                        <Cube/>
+                        <div className={styles.blocks}>
+                            <div className={styles.grid}
+                                 style={{gridTemplateColumns: '8px auto 8px', gridTemplateRows: '8px auto 12px'}}>
+                                {[...Array(9)].map((e, i) => i).map((i) => <div className={styles.atom} key={i}
+                                                                                style={{backgroundColor: `rgba(255, 241, 108, ${1 - i * 0.07})`}}></div>)}
+                            </div>
+                            <p className={styles.header}>grid-template-columns: 8px auto 8px;<br/>
+                                grid-template-rows: 8px auto 12px;
+                            </p>
+                        </div>
+                        <div className={styles.blocks}>
+                            <div className={styles.grid}
+                                 style={{gridTemplateColumns: '20% 20% auto', gridTemplateRows: '20% auto 20%'}}>
+                                {[...Array(9)].map((e, i) => i).map((i) => <div className={styles.atom} key={i}
+                                                                                style={{backgroundColor: `rgba(255, 241, 108, ${1 - i * 0.07})`}}></div>)}
+                            </div>
+                            <p className={styles.header}>grid-template-columns: 20% 20% auto;<br/>
+                                grid-template-rows: 20% auto 20%;
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -73,8 +61,6 @@ function Page() {
     );
 };
 
-Page.propTypes = {
-
-};
+Page.propTypes = {};
 
 export default Page;
---
+++ components/Grid/GridTemplate/GridTemplate.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styles from "../../../app/grid/grid.module.css";
+
+// @ts-ignore
+function GridTemplate(props) {
+    return (
+        <div className={styles.set}>
+            <div className={styles.header}>grid-template</div>
+            <div className={styles.lore}>
+                <p>Defines the rows and columns of the grid.</p>
+            </div>
+            <div className={styles.sections}>
+                <div className={styles.blocks}>
+                    <div className={styles.grid}
+                         style={{gridTemplateColumns: '12px 12px 12px', gridTemplateRows: '12px 12px 12px'}}>
+                        {[...Array(9)].map((e, i) => i).map((i) => <div className={styles.atom} key={i}
+                                                                        style={{backgroundColor: `rgba(255, 241, 108, ${1 - i * 0.07})`}}></div>)}
+                    </div>
+                    <p className={styles.header}>grid-template-columns: 12px 12px 12px;<br/>grid-template-rows:
+                        12px 12px 12px;
+                    </p>
+                </div>
+                <div className={styles.blocks}>
+                    <div className={styles.grid}
+                         style={{gridTemplateColumns: 'repeat(3, 1fr)', gridTemplateRows: 'repeat(3, 1fr)'}}>
+                        {[...Array(9)].map((e, i) => i).map((i) => <div className={styles.atom} key={i}
+                                                                        style={{backgroundColor: `rgba(255, 241, 108, ${1 - i * 0.07})`}}></div>)}
+                    </div>
+                    <p className={styles.header}>grid-template-columns: repeat(3, 1fr);<br/>grid-template-columns:
+                        repeat(3, 1fr)
+                    </p>
+                </div>
+                <div className={styles.blocks}>
+                    <div className={styles.grid}
+                         style={{gridTemplateColumns: '8px auto 8px', gridTemplateRows: '8px auto 12px'}}>
+                        {[...Array(9)].map((e, i) => i).map((i) => <div className={styles.atom} key={i}
+                                                                        style={{backgroundColor: `rgba(255, 241, 108, ${1 - i * 0.07})`}}></div>)}
+                    </div>
+                    <p className={styles.header}>grid-template-columns: 8px auto 8px;<br/>
+                        grid-template-rows: 8px auto 12px;
+                    </p>
+                </div>
+                <div className={styles.blocks}>
+                    <div className={styles.grid}
+                         style={{gridTemplateColumns: '20% 20% auto', gridTemplateRows: '20% auto 20%'}}>
+                        {[...Array(9)].map((e, i) => i).map((i) => <div className={styles.atom} key={i}
+                                                                        style={{backgroundColor: `rgba(255, 241, 108, ${1 - i * 0.07})`}}></div>)}
+                    </div>
+                    <p className={styles.header}>grid-template-columns: 20% 20% auto;<br/>
+                        grid-template-rows: 20% auto 20%;
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default GridTemplate;
