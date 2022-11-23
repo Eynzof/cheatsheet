0e465ef1cb2050d5f6da2e5e16178738de8d315a
enzo <Eynzof@gmail.com>
2022-11-23 15:23:36 +0800

    add tooltips

--- components/Grid/Grid/GridDisplay.tsx
+++ components/Grid/Grid/GridDisplay.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from "../../../styles/grid.module.css";
+import { Tooltip } from '@chakra-ui/react'
+import { QuestionOutlineIcon } from "@chakra-ui/icons";
 
 const GridDisplay = () => {
     return (
@@ -7,7 +9,7 @@ const GridDisplay = () => {
             <div className={styles.set}>
                 <div className={styles.header}>grid</div>
                 <div className={styles.lore}>
-                    <p>Defines the rows and columns of the grid.</p>
+                    <p>为子元素定义一个网格环境</p>
                 </div>
                 <div className={styles.sections}>
                     <div className={styles.blocks}>
@@ -19,45 +21,9 @@ const GridDisplay = () => {
                             {[...Array(9)].map((e, i) => i).map((i) => <div className={styles.atom} key={i}
                                                                             style={{backgroundColor: `rgba(255, 241, 108, ${1 - i * 0.07})`}}></div>)}
                         </div>
-                        <p className={styles.header}>display: grid;
-                        </p>
-                    </div>
-                </div>
-                <div className={styles.sections}>
-                    <div className={styles.blocks} >
-                        <p
-                            style={{borderBottom: '1px solid #aaa'}}
-                            className={styles.desc}>默认情况下 grid 是块级元素
-                        </p>
-                    </div>
-                </div>
-                <div className={styles.sections}>
-                    <div className={styles.blocks}>
-                        <div className={styles.grid}
-                             style={{
-                                 gridTemplateColumns: '12px 12px 12px',
-                                 gridTemplateRows: '12px 12px 12px',
-                             }}>
-                            {[...Array(9)].map((e, i) => i).map((i) => <div className={styles.atom} key={i}
-                                                                            style={{backgroundColor: `rgba(255, 241, 108, ${1 - i * 0.07})`}}></div>)}
-                        </div>
-                        <div className={styles.grid}
-                             style={{
-                                 gridTemplateColumns: '12px 12px 12px',
-                                 gridTemplateRows: '12px 12px 12px',
-                             }}>
-                            {[...Array(9)].map((e, i) => i).map((i) => <div className={styles.atom} key={i}
-                                                                            style={{backgroundColor: `rgba(255, 241, 108, ${1 - i * 0.07})`}}></div>)}
-                        </div>
-                        <p className={styles.header}>display: grid;
-                        </p>
-                    </div>
-                </div>
-                <div className={styles.sections}>
-                    <div className={styles.blocks}>
-                        <p
-                            style={{borderBottom: '1px solid #aaa'}}
-                            className={styles.desc}>改为行内元素
+                        <p className={styles.header}>display: grid; <Tooltip closeDelay={1500} label='默认情况下 grid 是块级元素，一个grid独占一行' fontSize='md'>
+                            <QuestionOutlineIcon />
+                        </Tooltip>
                         </p>
                     </div>
                 </div>
@@ -71,15 +37,33 @@ const GridDisplay = () => {
                             {[...Array(9)].map((e, i) => i).map((i) => <div className={styles.atom} key={i}
                                                                             style={{backgroundColor: `rgba(255, 241, 108, ${1 - i * 0.07})`}}></div>)}
                         </div>
-                        <p className={styles.header}>display: inline-grid;
+                        <div className={styles.grid}
+                             style={{
+                                 gridTemplateColumns: '12px 12px 12px',
+                                 gridTemplateRows: '12px 12px 12px',
+                             }}>
+                            {[...Array(9)].map((e, i) => i).map((i) => <div className={styles.atom} key={i}
+                                                                            style={{backgroundColor: `rgba(255, 241, 108, ${1 - i * 0.07})`}}></div>)}
+                        </div>
+                        <p className={styles.header}>display: inline-grid; <Tooltip closeDelay={1500} hasArrow  label='将 grid 改为行内元素，允许同一行内存在多个 grid' fontSize='md'>
+                            <QuestionOutlineIcon />
+                        </Tooltip>
                         </p>
                     </div>
                 </div>
                 <div className={styles.sections}>
                     <div className={styles.blocks}>
-                        <p
-                            className={styles.desc}
-                           style={{borderBottom: '1px solid #aaa'}}>详见 <a href="https://ishadeed.com/article/learn-css-subgrid/">Learn CSS Subgrid - Ahmad Shadeed</a>
+                        <div className={styles.grid}
+                             style={{
+                                 gridTemplateColumns: '12px 12px 12px',
+                                 gridTemplateRows: '12px 12px 12px',
+                             }}>
+                            {[...Array(9)].map((e, i) => i).map((i) => <div className={styles.atom} key={i}
+                                                                            style={{backgroundColor: `rgba(255, 241, 108, ${1 - i * 0.07})`}}></div>)}
+                        </div>
+                        <p className={styles.header}>display: sub-grid; <Tooltip closeDelay={1500} label='详见 https://ishadeed.com/article/learn-css-subgrid/' fontSize='md'>
+                            <QuestionOutlineIcon />
+                        </Tooltip>
                         </p>
                     </div>
                 </div>
